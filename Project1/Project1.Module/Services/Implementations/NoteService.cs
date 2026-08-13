4c08ce645fe722a36603a49e1a3668a0cfa45ac1
Veli Ünüşdü <veli.unusdu@cobantur.com>
2026-08-13 11:29:33 +0300

    fix(ui): organize Not_DetailView layout vertically and use INonSecuredObjectSpaceFactory in NoteService

--- Project1/Project1.Module/Services/Implementations/NoteService.cs
+++ Project1/Project1.Module/Services/Implementations/NoteService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
 using Project1.Module.BusinessObjects.Notes;
 using Project1.Module.DTOs;
 using Project1.Module.Services.Interfaces;
@@ -14,15 +15,28 @@ namespace Project1.Module.Services.Implementations
     public class NoteService : INoteService
     {
         private readonly IObjectSpaceFactory _objectSpaceFactory;
+        private readonly INonSecuredObjectSpaceFactory? _nonSecuredObjectSpaceFactory;
 
-        public NoteService(IObjectSpaceFactory objectSpaceFactory)
+        public NoteService(
+            IObjectSpaceFactory objectSpaceFactory,
+            INonSecuredObjectSpaceFactory? nonSecuredObjectSpaceFactory = null)
         {
             _objectSpaceFactory = objectSpaceFactory ?? throw new ArgumentNullException(nameof(objectSpaceFactory));
+            _nonSecuredObjectSpaceFactory = nonSecuredObjectSpaceFactory;
+        }
+
+        private IObjectSpace CreateObjectSpace()
+        {
+            if (_nonSecuredObjectSpaceFactory != null)
+            {
+                return _nonSecuredObjectSpaceFactory.CreateNonSecuredObjectSpace(typeof(Not));
+            }
+            return _objectSpaceFactory.CreateObjectSpace(typeof(Not));
         }
 
         public Task<IEnumerable<NoteDto>> GetNotesAsync(CancellationToken cancellationToken = default)
         {
-            using IObjectSpace objectSpace = _objectSpaceFactory.CreateObjectSpace(typeof(Not));
+            using IObjectSpace objectSpace = CreateObjectSpace();
             var notes = objectSpace.GetObjectsQuery<Not>()
                 .Select(n => new NoteDto(
                     n.Oid,
@@ -40,7 +54,7 @@ namespace Project1.Module.Services.Implementations
 
         public Task<NoteDto?> GetNoteByIdAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            using IObjectSpace objectSpace = _objectSpaceFactory.CreateObjectSpace(typeof(Not));
+            using IObjectSpace objectSpace = CreateObjectSpace();
             var n = objectSpace.GetObjectByKey<Not>(id);
             if (n == null) return Task.FromResult<NoteDto?>(null);
 
@@ -59,7 +73,7 @@ namespace Project1.Module.Services.Implementations
 
         public Task<NoteDto> CreateNoteAsync(CreateNoteRequestDto request, CancellationToken cancellationToken = default)
         {
-            using IObjectSpace objectSpace = _objectSpaceFactory.CreateObjectSpace(typeof(Not));
+            using IObjectSpace objectSpace = CreateObjectSpace();
             var not = objectSpace.CreateObject<Not>();
             not.Baslik = request.Baslik;
             not.Icerik = request.Icerik;
